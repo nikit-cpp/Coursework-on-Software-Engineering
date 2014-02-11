11ed7fb2e2688df8b2b75042510af9c2987d65a8
nikit.cpp <nikit.cpp@yandex.ru>
2014-02-11 21:47:36 +0300

    убрано наследование RubricDAO

--- src/main/java/engine/thematicdictionary/ThematicDicManager.java
+++ src/main/java/engine/thematicdictionary/ThematicDicManager.java
@@ -13,10 +13,52 @@ import engine.thematicdictionary.hibernate.DAO.RubricDAO;
 /**
  * Расширение RubricDAO для повышения юзабилити
  */
-public final class ThematicDicManager extends RubricDAO{
-	
+public final class ThematicDicManager /*extends RubricDAO*/{
+	private ArrayList<ThematicDic> thematicDicts;
+
 	public ThematicDicManager(){
 	}
+	
+	public ArrayList<ThematicDic> getAllDicts() {
+		return thematicDicts;
+	}
+
+	public void addDic(ThematicDic thematicDic) {
+		// TODO Auto-generated method stub
+		// session.save(new Rubric(thematicDic.getName()))
+		
+		updateDictsArrayList();
+	}
+
+	public ThematicDic getDic(int i) {
+		// TODO Auto-generated method stub
+		// TODO учесть то что в БД индекс начинается с 1, а в ArrayList с 0.
+		return null;
+	}
+
+	public void deleteDic(int dicIndex) {
+		// TODO Auto-generated method stub
+		// TODO учесть то что в БД индекс начинается с 1, а в ArrayList с 0.
+		updateDictsArrayList();
+	}
+	
+	
+	/**
+	 * Создаёт arrayList из полученных из БД объектов Rubric
+	 * @return
+	 */
+	private void updateDictsArrayList() {
+		// TODO Auto-generated method stub
+		//thematicDicts = new ArrayList<ThematicDic>();
+		//session.createCriteria();
+	}
+
+	
+	
+	
+	
+	
+	
 
 	public void addDic(String dicname, boolean isEnabled){
 		addDic(new ThematicDic(dicname, isEnabled));
